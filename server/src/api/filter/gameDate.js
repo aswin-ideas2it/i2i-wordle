df3542c29b57dcd72f8c3efe78eb367e55b48a9e
Maria Antony Praveen <mariaantony.praveenraja@ideas2it.com>
2022-09-21 14:44:25 +0530

    Updated filter

--- server/src/api/filter/gameDate.js
+++ server/src/api/filter/gameDate.js
@@ -4,13 +4,17 @@ const gameDate = async (req, res) => {
     try {
         const {
             date,
-            status
+            status,
+            showMinimimalData = false
         } = req.body;
+
+        const presentation = showMinimimalData ? { history: 0, isLocalUser: 0 } : {};
         let audit = await auditModel.find({
             [`history.${date}`]: { $exists: true },
-            [`history.${date}.status`]: status
-        }, { _id: 0, "log": 0 });
-        res.json(audit);
+            [`history.${date}.status`]: { $in: status instanceof Array ? status : [status] }
+        }, { ...{ _id: 0, "log": 0 }, ...presentation });
+        res.json({ count: audit.length, audit });
+
     } catch (error) {
         console.log(error, 'gameDate');
         const errorMsg = error.message ? error.message : error;
